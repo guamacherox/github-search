a69c684034822b7597dc3e3b90de189c06157f96
Juan Marval <juanluismarval@gmail.com>
2018-03-03 00:32:42 -0400

    cambios menores en interfaz, cambio de token a client_id u secret para conexión con git

---
+++ README.md
@@ -0,0 +1,21 @@
+# GitHub Search
+
+> Github Search App (Builded with AngularJS)
+
+## Build Setup
+
+``` bash
+
+# install dependencies
+npm install
+
+# to build an optimized version of your application in /dist
+npm run build
+
+# serve with hot reload at localhost:8080
+npm run serve
+
+# serve production version with hot reload at localhost:8080
+npm run serve:dist
+
+```
--- src/app/components/git-users/git-user.service.js
+++ src/app/components/git-users/git-user.service.js
@@ -4,12 +4,21 @@ export class GitUserService {
     'ngInject';
     this.$http = $http;
     this.ApiBase = ApiBase;
+    this.clientId = '7300bf3a22909989fdb5';
+    this.clientSecret = '47311167016524db192b6ee2550b0bdae57322e9';
   }
+  // getUsers(user) {
+  //   const URL = `${this.ApiBase.baseURL}/search/users?q=${user}&per_page=10`;
+  //   return this.$http.get(URL, {headers: {Authorization: `token ${this.ApiBase.token}`}});
+  // }
+  // getUserFollowers(url) {
+  //   return this.$http.get(`${url}?per_page=3`, {headers: {Authorization: `token ${this.ApiBase.token}`}});
+  // }
   getUsers(user) {
-    const URL = `${this.ApiBase.baseURL}/search/users?q=${user}&per_page=10`;
-    return this.$http.get(URL, {headers: {Authorization: `token ${this.ApiBase.token}`}});
+    const URL = `${this.ApiBase.baseURL}/search/users?q=${user}&per_page=10&client_id=${this.clientId}&client_secret=${this.clientSecret}`;
+    return this.$http.get(URL);
   }
   getUserFollowers(url) {
-    return this.$http.get(`${url}?per_page=3`, {headers: {Authorization: `token ${this.ApiBase.token}`}});
+    return this.$http.get(`${url}?per_page=3&client_id=${this.clientId}&client_secret=${this.clientSecret}`);
   }
 }
