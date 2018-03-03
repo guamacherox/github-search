4d49abb6c9d2bf00be3552c4406a1a2085e7e701
Juan Marval <juanluismarval@gmail.com>
2018-03-03 09:45:39 -0400

    # Eliminar Import Innecesario * Eliminado import de angular material.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import {App} from './app/App';
 import {gitUserTableComponent} from './app/components/git-users/git-user-table';
 import {GitUserService} from './app/components/git-users/git-user.service';
 import 'angular-ui-router';
-import 'angular-material';
-import 'angular-messages';
 import 'angular-animate';
+import 'angular-messages';
 import routesConfig from './routes';
 
 import 'bulma/css/bulma.css';
@@ -16,7 +15,7 @@ import './index.scss';
 export const app = 'app';
 
 angular
-  .module(app, ['ui.router', 'ngAnimate'])
+  .module(app, ['ui.router', 'ngAnimate', 'ngMessages'])
   .config(routesConfig)
   .constant('ApiBase', API_BASE)
   .service('GitUserService', GitUserService)
